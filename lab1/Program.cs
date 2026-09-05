9046511044a82478ac449f9aea8e88c59070eda4
timur456go <timur456go@gmail.com>
2026-09-05 10:10:51 +0300

    Add files via upload

---
+++ lab1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+class Task3_Method1
+{
+    static void Main()
+    {
+        int N = 8;
+        int expected = N - 1;
+        object locker = new object();
+
+        Thread[] threads = new Thread[N];
+        for (int i = 0; i < N; i++)
+        {
+            int id = i;
+            threads[i] = new Thread(() =>
+            {
+                while (true)
+                {
+                    lock (locker)
+                    {
+                        if (expected == id)
+                        {
+                            Console.WriteLine($"Поток {id}");
+                            expected--;
+                            Monitor.PulseAll(locker);
+                            return;
+                        }
+                        Monitor.Wait(locker);
+                    }
+                }
+            });
+            threads[i].Start();
+        }
+
+        foreach (Thread t in threads) t.Join();
+    }
+}
