4b74c8c6269c4ae768a5f4f90834d5e4154f3887
timur456go <timur456go@gmail.com>
2026-09-05 07:57:44 +0300

    Add files via upload

---
+++ lab1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+class A
+{
+    static void Main()
+    {
+        int numThreads = 8; 
+       
+        Thread[] threads = new Thread[numThreads];
+
+        for (int i = 0; i < numThreads; i++)
+        {
+            int threadID = i; 
+            threads[i] = new Thread(() =>
+            {
+                Console.WriteLine($"Поток {threadID+1} из {numThreads}");
+            });
+            threads[i].Start();
+        }
+
+        foreach (Thread t in threads)
+            t.Join();
+    }
+}
