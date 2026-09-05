8fc67377a018d2512e81bfb4d0919c0c5531620f
timur456go <timur456go@gmail.com>
2026-09-05 07:58:39 +0300

    Add files via upload

---
+++ lab1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+class Zad2_Dynamic
+{
+    static void Main()
+    {
+        const int N = 16000;
+        double[] a = new double[N];
+        double[] b = new double[N];
+        for (int i = 0; i < N; i++) a[i] = i;
+
+        int numThreads = 8;
+        int nextIndex = 1;       
+        object locker = new object();
+
+        Thread[] threads = new Thread[numThreads];
+        for (int t = 0; t < numThreads; t++)
+        {
+            threads[t] = new Thread(() =>
+            {
+                while (true)
+                {
+                    int i;
+                    lock (locker)
+                    {
+                        if (nextIndex >= N - 1) break; 
+                        i = nextIndex;
+                        nextIndex++;
+                    }
+                    b[i] = (a[i - 1] + a[i] + a[i + 1]) / 3.0;
+                    Console.WriteLine($"Поток {t}: {b[i]}");
+                }
+            });
+            threads[t].Start();
+        }
+
+        foreach (Thread th in threads) th.Join();
+        Console.WriteLine("Динамическое распределение завершено");
+    }
+}
